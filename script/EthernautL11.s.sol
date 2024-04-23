5af87a0a8c207da8f1e540b672e747a8bf4e7868
SamijoeHayek <samijoehayek1@gmail.com>
2024-04-23 20:42:20 +0300

    Added template lvl 11

--- script/EthernautL11.s.sol
+++ script/EthernautL11.s.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
 
 import {Script, console} from "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract EthernautL11Script is Script {
     function setUp() public {}
 
     function run() public {
